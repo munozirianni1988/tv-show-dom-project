434f1c61bb5c0ecfa73688dbfd69684b15c56394
Irianni Munoz <82213740+munozirianni1988@users.noreply.github.com>
2023-05-06 15:26:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //You can edit ALL of the code here
+
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
@@ -6,7 +7,18 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  //rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  episodeList.forEach((element) => {
+    let headerElement = document.createElement("h3");
+    let imgElement = document.createElement("img");
+    let pElement = document.createElement("p");
+    headerElement.innerHTML = element.name;
+    imgElement.src = element.image.medium;
+    pElement.innerHTML = element.summary;
+    rootElem.appendChild(headerElement);
+    rootElem.appendChild(imgElement);
+    rootElem.appendChild(pElement);
+  });
 }
 
 window.onload = setup;
